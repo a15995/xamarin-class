946cb8bec0ac147b3c8b20ae3bb704803400a760
SL <SL>
2016-11-09 21:11:01 +0100

    NameClass

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApplication4
+{
+    public class AnimalSpecies3D
+    {
+        public string name
+        public int legs, eyes;
+
+        public AnimalSpecies3D(string name, int legs, int eyes)
+    }
+    public class Pet2D
+    {
+        public string species, name;
+
+        public Pet2D(string species, string name)
+    }
+    public class Person3D
+    {
+        public string name, birth, pet;
+
+        public Person3D(string name, string birth, string pet)
+    }
+}
